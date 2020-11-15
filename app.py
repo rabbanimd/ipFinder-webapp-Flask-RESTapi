d7fae3607a59a4a81e43fbacd62109515d3d3522
cortes anibal <c0rt3s@k4l1.k4l1>
2020-11-15 12:27:23 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import sqlite3
+from flask import Flask,render_template,redirect,url_for,request,jsonify,flash
+from requests import get
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+DEBUG=True
+
+# con.execute("create table ip_addr(ip TEXT NOT NULL,version TEXT,country TEXT,country_capital TEXT,city TEXT,region TEXT,country_code TEXT, currency TEXT, org TEXT)")
+
+@app.route('/',methods=["POST","GET"])
+def home():
+    if request.method=="POST":
+        ip=request.form["ip"]
+        url = 'https://ipapi.co/{}/json/'
+        loc = get(url.format(ip))
+        res = loc.json()
+        if 'country' not in res:
+            flash("This Ip address doesn't exist.")
+        return render_template("index.html", data=res)
+
+
+    else:
+        url='http://bot.whatismyipaddress.com/'
+        loc=get(url)
+        data = loc.content
+        ip = data.decode("utf-8")
+        url = 'https://ipapi.co/{}/json/'
+        loc = get(url.format(ip))
+        res = loc.json()
+        return render_template("index.html", data=res)
+
+if __name__ == '__main__':
+    app.run(debug=True)
